f364f5f8b983ef0c46ed1bdc923e1ed39be3d594
Ulf Leopold <336dev@gmail.com>
2016-09-21 20:42:47 +0200

    Remove support for haskell and scheme configs for now. Load time for a simple .hs or .scm config is about 0.5 to 1 second compared to a few milliseconds for lua or markdown files.

--- src/Hub/Config.hs
+++ src/Hub/Config.hs
@@ -14,33 +14,20 @@ import Text.Printf (printf)
 import System.CPUTime
 import Data.Either
 
-import qualified Language.Haskell.Interpreter as I
-import qualified Language.Scheme.Core as S
-
 import Hub.CommandType
 import Hub.ConfigMarkdown
 import Hub.ConfigLua
 
 readConfig :: AppConfig -> IO [Command]
 readConfig appCfg = do
-    cfgFiles <- getConfigFiles appCfg
-    cfgFile <- firstExisting cfgFiles
+    cfgFile <- configFile appCfg
     case cfgFile of
         Just file ->
             dt
                 appCfg
                 "Time spent loading configs: %0.3f sec\n"
-                (\_ -> fileToCmds file)
+                (\_ -> loadCommands file)
         Nothing -> return []
-
-firstExisting :: [FilePath] -> IO (Maybe FilePath)
-firstExisting [] = return Nothing
-firstExisting (filePath : filePaths) = do
-  res <- doesFileExist filePath
-  if res then
-      return (Just filePath)
-  else
-      firstExisting filePaths
 
 filterCmds :: [String] -> [Command] -> [Command]
 filterCmds keywords cmds =
@@ -60,7 +47,9 @@ getShellCmd :: Command -> String
 getShellCmd (Command tags shellCmd) = shellCmd
 
 -- Internal ============================================================
-getConfigFiles appCfg = do
+configFile appCfg = do fileCandidates appCfg >>= firstExisting
+
+fileCandidates appCfg = do
     home <- getHomeDirectory
     let defaultFiles =
             map (\x -> joinPath [home, x]) [".hub.lua", ".hub.md"]
@@ -68,13 +57,20 @@ getConfigFiles appCfg = do
       "" -> return defaultFiles
       x -> return [x]
 
-fileToCmds :: FilePath -> IO [Command]
-fileToCmds filepath = do
+firstExisting :: [FilePath] -> IO (Maybe FilePath)
+firstExisting [] = return Nothing
+firstExisting (filePath : filePaths) = do
+  res <- doesFileExist filePath
+  if res then
+      return (Just filePath)
+  else
+      firstExisting filePaths
+
+loadCommands :: FilePath -> IO [Command]
+loadCommands filepath = do
     case takeExtension filepath of
         ".lua" -> luaFileToCmds filepath
         ".md" -> markdownFileToCmds filepath
-        ".scm" -> schemeFileToCmds filepath
-        ".hs" -> haskellFileToCmds filepath
         _ -> error "Not supported config file extension."
 
 dt (AppConfig {profile = True}) msg fun = do
@@ -86,48 +82,3 @@ dt (AppConfig {profile = True}) msg fun = do
     return res
 dt (AppConfig {profile = False}) msg fun = 
     fun ()
-
-haskellFileToCmds :: FilePath -> IO [Command]
-haskellFileToCmds filePath = do
-    config <- interpretHaskell filePath
-    toCommandList config
-
-interpretHaskell :: FilePath -> IO [(String, [String], String)]
-interpretHaskell filePath = do
-    res <-
-        I.runInterpreter $
-        do I.loadModules [filePath]
-           I.setImports ["Prelude", "HubConfig"]
-           I.interpret
-                "HubConfig.main"
-                (I.as :: [(String, [String], String)])
-    case res of
-        Left err ->
-            error (printf "Couldn't evaluate Hub config (%s)." (show err))
-        Right x -> return x
-
-schemeFileToCmds :: FilePath -> IO [Command]
-schemeFileToCmds filePath = do
-    config <- interpretScheme filePath
-    toCommandList config
-
-interpretScheme :: FilePath -> IO [(String, [String], String)]
-interpretScheme filePath = do
-  contents <- readFile filePath
-  env <- S.r5rsEnv
-  res <- S.evalString env contents
-  return [("Command", [], "ssh")]
-
-toCommandList :: [(String, [String], String)] -> IO [Command]
-toCommandList [] = return []
-toCommandList (("Command", tags, cmd) : rest)  = do
-  res <- toCommandList rest
-  return ((Command tags cmd) : res)
-toCommandList (("Include", tags, includePath) : rest) = do
-  res <- fileToCmds includePath
-  let resWithTags = addTags tags res
-  cmds2 <- toCommandList rest
-  return (resWithTags ++ cmds2)
-
-addTags tags cmds =
-    map (\(Command t cmd) -> (Command (tags ++ t) cmd)) cmds
