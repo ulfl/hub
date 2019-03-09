0f083be71bdeb753b9d7870f0138a4ccde7e853f
Ulf Leopold <336dev@gmail.com>
2019-03-09 12:51:05 +0100

    Refactor imports.

--- src/Hub/Hub.hs
+++ src/Hub/Hub.hs
@@ -2,205 +2,251 @@
 --
 {-# OPTIONS_GHC -Wno-name-shadowing #-}
 
-module Hub.Hub (hub) where
+module Hub.Hub
+  ( hub
+  ) where
 
-import Hub.CmdLine
-
-import qualified Graphics.Vty as V
-import qualified Brick.Main as M
-import qualified Brick.Types as T
-import qualified Brick.Widgets.List as L
-import qualified Brick.Widgets.Edit as E
-import qualified Brick.AttrMap as A
-import qualified Data.Vector as Vec
-import qualified Data.Char
+import Brick.AttrMap (AttrMap, AttrName, attrMap)
+import Brick.Main (App(..), continue, defaultMain, halt, showFirstCursor)
+import Brick.Types (BrickEvent(..), EventM, Next, Padding(..))
 import Brick.Types (Widget)
-import Brick.Widgets.Core
-       (str, vLimit, hLimit, hBox, vBox, withAttr, padLeft, padRight,
-        fill)
 import Brick.Util (on)
-import qualified Data.Text.Zipper as Z
-import qualified Text.Printf
-
-import System.Process
-
-import Hub.CommandType
-import qualified Hub.Config as Hc
-
+import Brick.Widgets.Core
+  ( fill
+  , hBox
+  , hLimit
+  , padLeft
+  , padRight
+  , str
+  , vBox
+  , vLimit
+  , withAttr
+  )
+import Brick.Widgets.Edit
+  ( Editor
+  , applyEdit
+  , editor
+  , getEditContents
+  , handleEditorEvent
+  , renderEditor
+  )
+import Brick.Widgets.List
+  ( List
+  , list
+  , listAttr
+  , listElementsL
+  , listMoveDown
+  , listMoveUp
+  , listReplace
+  , listSelectedAttr
+  , listSelectedElement
+  , renderList
+  )
 import Control.Lens ((^.))
+import Data.Char (isSpace)
+import Data.Text.Zipper (deletePrevChar)
+import Data.Vector (fromList)
+import Graphics.Vty
+  ( Event(..)
+  , Key(..)
+  , Modifier(..)
+  , defAttr
+  , defaultStyleMask
+  , red
+  , reverseVideo
+  , white
+  , withStyle
+  )
+import Hub.CmdLine
+import Hub.CommandType
+  ( Command
+  , filterCmdsAndTags
+  , getShellCmd
+  , isPartialMatchTag
+  , mapCmds
+  )
+import Hub.Config (readConfig)
+import System.Process (callCommand)
+import Text.Printf (printf)
 
 data FieldName
-    = ListField
-    | EditField
-     deriving (Ord, Show, Eq)
+  = ListField
+  | EditField
+  deriving (Ord, Show, Eq)
 
 data State =
-    State (L.List FieldName ListRow)  -- The list widget.
-          (E.Editor String FieldName) -- The editor widget.
-          [Command]                   -- List of available 'Commands'.
-          Action                      -- Action to take on exit.
+  State (List FieldName ListRow) -- The list widget.
+        (Editor String FieldName) -- The editor widget.
+        [Command] -- List of available 'Commands'.
+        Action -- Action to take on exit.
 
-data ListRow = ListRow String String deriving (Ord, Show, Eq)
+data ListRow =
+  ListRow String
+          String
+  deriving (Ord, Show, Eq)
 
 type Cmd = String
-data Action = Run Cmd | Print Cmd | JustExit
+
+data Action
+  = Run Cmd
+  | Print Cmd
+  | JustExit
 
 hub :: IO ()
 hub = do
-    appCfg <- getAppConfig
-    cmds <- Hc.readConfig appCfg
-    let filteredCmds = filterCmdsAndTags (tags appCfg) cmds
-    cmd <-
-        case filteredCmds of
-            [cmd] -> return (Run (getShellCmd cmd))
-            _ -> do
-                State _ _ _ cmd <-
-                    M.defaultMain theApp (initialState filteredCmds)
-                return cmd
-    action appCfg cmd
+  appCfg <- getAppConfig
+  cmds <- readConfig appCfg
+  let filteredCmds = filterCmdsAndTags (tags appCfg) cmds
+  cmd <-
+    case filteredCmds of
+      [cmd] -> return (Run (getShellCmd cmd))
+      _ -> do
+        State _ _ _ cmd <- defaultMain theApp (initialState filteredCmds)
+        return cmd
+  action appCfg cmd
 
 action :: AppConfig -> Action -> IO ()
 action appCfg (Run cmd) =
-    if not (dryrun appCfg) then
-        callCommand cmd
-    else
-        putStrLn cmd
+  if not (dryrun appCfg)
+    then callCommand cmd
+    else putStrLn cmd
 action _ (Print cmd) = putStrLn cmd
 action _ JustExit = return ()
 
 -- Internal ============================================================
 initialState :: [Command] -> State
 initialState cmds =
-    State
-        (L.list ListField (Vec.fromList (commandsToRows cmds)) 2)
-        (E.editor EditField (Just 1) "")
-        cmds
-        JustExit
+  State
+    (list ListField (fromList (commandsToRows cmds)) 2)
+    (editor EditField (Just 1) "")
+    cmds
+    JustExit
 
-theApp :: M.App State V.Event FieldName
+theApp :: App State Graphics.Vty.Event FieldName
 theApp =
-    M.App
-    { M.appDraw = drawUI
-    , M.appChooseCursor = M.showFirstCursor
-    , M.appHandleEvent = appEvent
-    , M.appStartEvent = return
-    , M.appAttrMap = const theAttrMap
+  App
+    { appDraw = drawUI
+    , appChooseCursor = showFirstCursor
+    , appHandleEvent = appEvent
+    , appStartEvent = return
+    , appAttrMap = const theAttrMap
     }
 
 drawUI :: State -> [Widget FieldName]
 drawUI state = [ui]
   where
     State l e cmds _ = state
-    box = L.renderList listDrawElement False l
-    prompt = E.renderEditor (str . unlines) True e
+    box = renderList listDrawElement False l
+    prompt = renderEditor (str . unlines) True e
     ui =
-        vBox
-            [ box
-            , hBox
-                  [ withAttr
-                        L.listSelectedAttr
-                        (str
-                             (Text.Printf.printf
-                                  "Showing %d of %d items."
-                                  (Vec.length (l^.L.listElementsL))
-                                  (length cmds)))
-                  , withAttr L.listSelectedAttr (vLimit 1 (fill ' '))]
-            , hBox [str "hub> ", prompt]]
+      vBox
+        [ box
+        , hBox
+            [ withAttr
+                listSelectedAttr
+                (str
+                   (Text.Printf.printf
+                      "Showing %d of %d items."
+                      (length (l ^. listElementsL))
+                      (length cmds)))
+            , withAttr listSelectedAttr (vLimit 1 (fill ' '))
+            ]
+        , hBox [str "hub> ", prompt]
+        ]
 
 listDrawElement :: Bool -> ListRow -> Widget FieldName
 listDrawElement sel (ListRow tags description) =
-    let attr =
-            if sel
-                then customAttr
-                else L.listAttr
-    in hBox
-           [ withAttr attr (vLimit 2 (hLimit 1 (fill ' ')))
-           , vLimit 2 (hLimit 1 (fill ' '))
-           , padRight
-                 T.Max
-                 (vBox [str tags, padLeft (T.Pad 8) (str description)])]
+  let attr =
+        if sel
+          then customAttr
+          else listAttr
+   in hBox
+        [ withAttr attr (vLimit 2 (hLimit 1 (fill ' ')))
+        , vLimit 2 (hLimit 1 (fill ' '))
+        , padRight Max (vBox [str tags, padLeft (Pad 8) (str description)])
+        ]
 
-appEvent :: State -> T.BrickEvent FieldName e -> T.EventM FieldName (T.Next State)
-appEvent (State l ed commands action) be = case be of
-    T.VtyEvent e -> case e of
-        V.EvKey (V.KChar 'n') [V.MCtrl] ->
-            M.continue (State (L.listMoveDown l) ed commands action)
-        V.EvKey V.KDown [] ->
-            M.continue (State (L.listMoveDown l) ed commands action)
-        V.EvKey (V.KChar 'p') [V.MCtrl] ->
-            M.continue (State (L.listMoveUp l) ed commands action)
-        V.EvKey V.KUp [] -> M.continue (State (L.listMoveUp l) ed commands action)
-        V.EvKey (V.KChar 'w') [V.MCtrl] ->
-            let words = head (E.getEditContents ed)
-                len = lengthOfPrevWord words
-                ed2 =
-                    foldl (\a _ -> E.applyEdit Z.deletePrevChar a) ed [1 .. len]
-                l2 = updateDisplayList l ed2 commands
-            in M.continue (State l2 ed2 commands action)
-        V.EvKey V.KEnter [] ->
-            let action =
-                    case L.listSelectedElement l of
-                        Just (_, ListRow _ cmd) -> Run cmd
-                        Nothing -> JustExit
-            in M.halt (State l ed commands action)
-        V.EvKey V.KEsc [] ->
-            let action =
-                    case L.listSelectedElement l of
-                        Just (_, ListRow _ cmd) -> Print cmd
-                        Nothing -> JustExit
-            in M.halt (State l ed commands action)
-        V.EvKey (V.KChar 'c') [V.MCtrl] -> M.halt (State l ed commands JustExit)
+appEvent :: State -> BrickEvent FieldName e -> EventM FieldName (Next State)
+appEvent (State l ed commands action) be =
+  case be of
+    VtyEvent e ->
+      case e of
+        EvKey (KChar 'n') [MCtrl] ->
+          continue (State (listMoveDown l) ed commands action)
+        EvKey KDown [] -> continue (State (listMoveDown l) ed commands action)
+        EvKey (KChar 'p') [MCtrl] ->
+          continue (State (listMoveUp l) ed commands action)
+        EvKey KUp [] -> continue (State (listMoveUp l) ed commands action)
+        EvKey (KChar 'w') [MCtrl] ->
+          let words = head (getEditContents ed)
+              len = lengthOfPrevWord words
+              ed2 = foldl (\a _ -> applyEdit deletePrevChar a) ed [1 .. len]
+              l2 = updateDisplayList l ed2 commands
+           in continue (State l2 ed2 commands action)
+        EvKey KEnter [] ->
+          let action =
+                case listSelectedElement l of
+                  Just (_, ListRow _ cmd) -> Run cmd
+                  Nothing -> JustExit
+           in halt (State l ed commands action)
+        EvKey KEsc [] ->
+          let action =
+                case listSelectedElement l of
+                  Just (_, ListRow _ cmd) -> Print cmd
+                  Nothing -> JustExit
+           in halt (State l ed commands action)
+        EvKey (KChar 'c') [MCtrl] -> halt (State l ed commands JustExit)
         _ -> do
-            ed2 <- E.handleEditorEvent e ed
-            let l2 = updateDisplayList l ed2 commands
-            M.continue (State l2 ed2 commands action)
-    _ -> M.continue (State l ed commands action)
+          ed2 <- handleEditorEvent e ed
+          let l2 = updateDisplayList l ed2 commands
+          continue (State l2 ed2 commands action)
+    _ -> continue (State l ed commands action)
 
 lengthOfPrevWord :: String -> Int
 lengthOfPrevWord str =
-    let str1 =
-            dropWhile
-                (not . Data.Char.isSpace)
-                (dropWhile Data.Char.isSpace (reverse str))
-    in length str - length str1
+  let str1 =
+        dropWhile
+          (not . Data.Char.isSpace)
+          (dropWhile Data.Char.isSpace (reverse str))
+   in length str - length str1
 
-updateDisplayList
-    :: L.List FieldName ListRow
-    -> E.Editor String FieldName
-    -> [Command]
-    -> L.List FieldName ListRow
+updateDisplayList ::
+     List FieldName ListRow
+  -> Editor String FieldName
+  -> [Command]
+  -> List FieldName ListRow
 updateDisplayList l ed commands =
-    L.listReplace
-        (Vec.fromList
-             (let words = getUserInputWords (head (E.getEditContents ed))
-              in commandsToRows (filterCmdsAndTags words commands)))
-        (Just 0)
-        l
+  listReplace
+    (fromList
+       (let words = getUserInputWords (head (getEditContents ed))
+         in commandsToRows (filterCmdsAndTags words commands)))
+    (Just 0)
+    l
 
 getUserInputWords :: String -> [String]
 getUserInputWords [] = []
 getUserInputWords s =
-    let wordList = words s
-        completed = last s == ' ' || isLastTagPartialMatch wordList
-    in if completed
-           then wordList
-           else init wordList
+  let wordList = words s
+      completed = last s == ' ' || isLastTagPartialMatch wordList
+   in if completed
+        then wordList
+        else init wordList
 
 isLastTagPartialMatch :: [String] -> Bool
 isLastTagPartialMatch [] = False
 isLastTagPartialMatch tags = isPartialMatchTag (last tags)
 
 commandsToRows :: [Command] -> [ListRow]
-commandsToRows commands =
-    mapCmds commands ListRow
+commandsToRows commands = mapCmds commands ListRow
 
-theAttrMap :: A.AttrMap
+theAttrMap :: AttrMap
 theAttrMap =
-    A.attrMap
-        V.defAttr
-        [ (L.listAttr, V.defAttr)
-        , (L.listSelectedAttr, V.defAttr `V.withStyle` V.reverseVideo)
-        , (customAttr, V.white `on` V.red `V.withStyle` V.defaultStyleMask)]
+  attrMap
+    defAttr
+    [ (listAttr, defAttr)
+    , (listSelectedAttr, defAttr `withStyle` reverseVideo)
+    , (customAttr, white `on` red `withStyle` defaultStyleMask)
+    ]
 
-customAttr :: A.AttrName
-customAttr = L.listSelectedAttr <> "custom"
+customAttr :: AttrName
+customAttr = listSelectedAttr <> "custom"
