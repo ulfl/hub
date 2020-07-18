249e7e922b80545b4dab60045bad4e855c95156f
Ulf Leopold <336dev@gmail.com>
2020-07-18 20:58:13 +0200

    Let user config pull in the prelude.

--- example/hub.dhall
+++ example/hub.dhall
@@ -1,18 +1,20 @@
+let h =
+      https://raw.githubusercontent.com/ulfl/hub/master/dhall/hub-prelude.dhall sha256:e95265ddad2a050372e803d519cc916ea3c72247fd6fca7457078f2e96e4f150
+
 let bookshop =
         λ(dns : Text)
-      →   [ web [ "splunk" ] ("https://splunk.intraweb.net?search=" ++ dns) ]
-        # cmds
+      →   [ h.web [ "splunk" ] ("https://splunk.intraweb.net?search=" ++ dns) ]
+        # h.cmds
           [ "ssh" ]
           [ "host1", "host2", "host3" ]
           (λ(arg : Text) → "ssh ${arg}." ++ dns)
 
-let config
-    : List Command
-    = tags
+let config =
+      h.tags
       [ "bookshop" ]
-      (   tags [ "staging" ] (bookshop "staging.net")
-        # tags [ "production eu" ] (bookshop "production-eu.com")
-        # tags [ "production us" ] (bookshop "production-us.com")
+      (   h.tags [ "staging" ] (bookshop "staging.net")
+        # h.tags [ "production eu" ] (bookshop "production-eu.com")
+        # h.tags [ "production us" ] (bookshop "production-us.com")
       )
 
 in  config
--- src/Hub/ConfigDhall.hs
+++ src/Hub/ConfigDhall.hs
@@ -13,46 +13,4 @@ import Hub.CommandType (Command)
 dhallFileToCmds :: FilePath -> IO [Command]
 dhallFileToCmds filePath = do
   config <- Data.Text.IO.readFile filePath
-  commands <- detailed (input auto (prelude <> config))
-  return commands
-
-prelude :: Text
-prelude =
-  [here|
-let map
-        : ∀(a : Type) → ∀(b : Type) → (a → b) → List a → List b
-        =   λ(a : Type)
-          → λ(b : Type)
-          → λ(f : a → b)
-          → λ(xs : List a)
-          → List/build
-            b
-            (   λ(list : Type)
-              → λ(cons : b → list → list)
-              → List/fold a xs list (λ(x : a) → cons (f x))
-            )
-
-let Command : Type = { tags : List Text, shellCommand : Text }
-
-let cmd = λ(t : List Text) → λ(c : Text) → { tags = t, shellCommand = c }
-
-let tags =
-        λ(t : List Text)
-      → λ(cmds : List Command)
-      → map
-        Command
-        Command
-        (λ(c : Command) → { tags = t # c.tags, shellCommand = c.shellCommand })
-        cmds
-
-let cmds =
-        λ(tags : List Text)
-      → λ(vars : List Text)
-      → λ(fmt : Text → Text)
-      → map Text Command (λ(var : Text) → cmd tags (fmt var)) vars
-
-let web =
-        λ(t : List Text)
-      → λ(c : Text)
-      → { tags = t, shellCommand = (env:OPENCMD as Text ? "open") ++ " " ++ c }
-|]
+  detailed (input auto config)
